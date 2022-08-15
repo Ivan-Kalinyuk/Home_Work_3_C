71b580a33eeccafe2ce8065e348c79aed58a1834
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-16 02:54:28 +0300

    Решили задачу, проверяющую, является ли число палиндромом

---
+++ №21/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите натуральное пятизначное число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+string b = Convert.ToString(a);
+if ( b[0] ==  b[4] |  b[1] ==  b[3] ) Console.Write("Ваше число является палиндромом");
+else Console.Write("Ваше число не является палиндромом");
