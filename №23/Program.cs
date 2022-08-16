e9373e12a7863127d85f1ac53a383087592e8361
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-16 03:36:56 +0300

    Решили задачу, выводящую на экран таблицу кубов от 1 до того числа, которое ввел пользователь

---
+++ №23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите любое натуральное число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+string Method4 (int a)
+{
+    string result = String.Empty;
+    while (a >= i)
+    {
+        int b = i*i*i;
+        string q = Convert.ToString(b);
+        result = result + q;
+        result = result + " ";
+        i++;
+    }
+    return result;
+}
+string res = Method4 (a);
+Console.Write("Таблица кубов от 1 до Вашего числа: ");
+Console.WriteLine(res);
